180ff470edcba91c133c43796c97c1c4097b61c5
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-04-17 11:11:18 +0200

    feat: subject mockup done

--- src/pages/Teacher/Subjects/Subjects.jsx
+++ src/pages/Teacher/Subjects/Subjects.jsx
@@ -1,38 +1,36 @@
 import { CalendarIcon } from "@heroicons/react/24/outline";
+import Folder from "../../../components/Teacher/Subjects/Folder";
 
 function Subjects() {
+  const subjects = [
+    "Math",
+    "M + U",
+    "Englisch",
+    "Deutsch",
+    "BG",
+    "Sport",
+    "Musik",
+  ];
+
   return (
     <>
       <div className=" mx-auto md:py-12 py-6 px-12 lg:px-8">
-        <p
-          className={`rounded-button  px-4 py-2 text-sm font-semibold  shadow-sm w-min  bg-teacherPrimary`}
-        >
-          Mathematik
-        </p>
-        <div className="grid grid-cols-12">
-          <div className="col-span-6">
-            <div>
-              <div className="flex">
-                <CalendarIcon className={"w-6 mr-2"} />
-                <p>Dateinen</p>
+        <div className="flex justify-between">
+          <p className="text-h-xl my-4">Fächer</p>
+          <button
+            type="button"
+            className={`rounded-button  px-2 py-2 text-sm  h-fit shadow-sm text-white  bg-teacherDarkBlue  `}
+          >
+            Fach hinzufügen
+          </button>
+        </div>
+        <div className="w-full  bg-teacherSecondary rounded-main mt-8 p-8">
+          <div className="p-8 rounded-main bg-teacherTertiary grid grid-cols-5">
+            {subjects.map((item, index) => (
+              <div className="p-4" key={index}>
+                <Folder name={item} />
               </div>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="you@example.com"
-              />
-            </div>
-            <div className="w-full bg-teacherSecondary mt-6 rounded-main p-6">
-              <button
-                type="button"
-                className={`rounded-button  px-4 py-2 text-sm font-semibold  shadow-sm  bg-teacherPrimary
-               `}
-              >
-                Aufgabe
-              </button>
-            </div>
+            ))}
           </div>
         </div>
       </div>
